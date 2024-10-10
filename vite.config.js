1cf1ff65f122ea43a5c7bc696490b28d0be9523f
lina98st <alina29st@gmail.com>
2024-10-10 11:50:58 +0200

    Titel in index.html aktualisiert

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,12 @@ import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
-export default defineConfig({
-  base: '/myquicklinks-vu/',  // Name deines Repos
-  plugins: [vue()],
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/myquicklinks-vu/',  // Dies sollte der Name deines Repos sein
   plugins: [
     vue(),
   ],
-
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
