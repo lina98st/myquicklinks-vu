da85b241b729cd0c697737b5746de3b1538f43f4
Alina <156576799+lina98st@users.noreply.github.com>
2024-10-10 10:59:08 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/myquicklinks-vue/',  // Dies sollte der Name deines Repos sein
+  base: '/myquicklinks-vu/',  // Dies sollte der Name deines Repos sein
   plugins: [
     vue(),
   ],
