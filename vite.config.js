dec7ed81b9129afc2606faaf08ea0e842c2b15d8
Alina <156576799+lina98st@users.noreply.github.com>
2024-10-10 10:31:36 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,17 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/myquicklinks-vue/',  // Dies sollte der Name deines Repos sein
   plugins: [
     vue(),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+});
+
